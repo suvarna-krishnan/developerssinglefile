ffe9c530d6dd6fb092f00c09aa7352c4dbe98a15
saravanakumar80 <72588594+saravanakumar80@users.noreply.github.com>
2021-10-22 19:16:22 +0530

    Update genesyscloud_routing_queue.tf

--- modules/queues/genesyscloud_routing_queue.tf
+++ modules/queues/genesyscloud_routing_queue.tf
@@ -7,19 +7,4 @@ terraform {
   }
 }
 
-resource "genesyscloud_routing_queue" "demo1" {
-  name                              = "demo1"
-  description                       = "demo1 team_demo_test"
-  acw_wrapup_prompt                 = "MANDATORY_TIMEOUT"
-  acw_timeout_ms                    = 300000
-  skill_evaluation_method           = "BEST"
-  auto_answer_only                  = true
-  enable_transcription              = true
-  enable_manual_assignment          = true
-  calling_party_name                = "demo12"
-  
-  media_settings_call {
-    alerting_timeout_sec      = 30
-    service_level_percentage  = 0.7
-    service_level_duration_ms = 10000
-  }
+
