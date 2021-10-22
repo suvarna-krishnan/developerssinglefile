beca52bdd48e931b0bb707b9b004026ed8921f2a
saravanakumar80 <72588594+saravanakumar80@users.noreply.github.com>
2021-10-22 19:13:10 +0530

    Update genesyscloud_routing_queue.tf

--- modules/queues/genesyscloud_routing_queue.tf
+++ modules/queues/genesyscloud_routing_queue.tf
@@ -23,3 +23,21 @@ resource "genesyscloud_routing_queue" "Retirements" {
     service_level_percentage  = 0.7
     service_level_duration_ms = 10000
   }
+  
+  
+  resource "genesyscloud_routing_queue" "Queues" {
+  name                              = "XYZ"
+  description                       = "XYZ team_demo_test"
+  acw_wrapup_prompt                 = "MANDATORY_TIMEOUT"
+  acw_timeout_ms                    = 300000
+  skill_evaluation_method           = "BEST"
+  auto_answer_only                  = true
+  enable_transcription              = true
+  enable_manual_assignment          = true
+  calling_party_name                = "Examples"
+  
+  media_settings_call {
+    alerting_timeout_sec      = 30
+    service_level_percentage  = 0.7
+    service_level_duration_ms = 10000
+  }
